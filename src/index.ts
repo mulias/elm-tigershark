7ea7be062d988c99e92bd34e948344f30c0f1a4a
Elias Mulhall <eli.mulhall@gmail.com>
2020-03-04 22:01:20 -0500

    Write cli script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Thanks for stopping by!");
+import { Elm } from "./Main.elm";
+import * as fs from "fs";
+import * as path from "path";
+
+const [_scriptRunner, _script, ...args] = process.argv;
+
+const versionFlag = args.includes("--version");
+const inputArgs = args.filter(s => !s.startsWith("--"));
+const outputArgs = args.filter(s => s.startsWith("--output="));
+
+if (versionFlag) {
+  const version = require("../package.json")["version"];
+  console.log(version);
+  process.exit(0);
+}
+
+const inputInvalid = inputArgs.length !== 1 || !inputArgs[0].endsWith(".elm");
+const outputInvalid =
+  outputArgs.length !== 1 || !outputArgs[0].endsWith(".d.ts");
+
+if (inputInvalid || outputInvalid) {
+  process.exit(1);
+}
+
+const inputFile = inputArgs[0];
+const outputFile = outputArgs[0].replace(/^--output=/, "");
+
+const inputFileSource = fs.readFileSync(inputFile).toString();
+
+const program = Elm.Main.init({ flags: { inputFileSource } });
+
+program.ports.writeFile.subscribe((declarations: string) => {
+  const outputFolder = path.dirname(outputFile);
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
+  fs.writeFileSync(outputFile, declarations);
+  process.exit(0);
+});
