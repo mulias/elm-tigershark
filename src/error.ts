665200379cf64e9d64f4b8d5ec2c28f1f9e939bf
Elias Mulhall <eli.mulhall@gmail.com>
2020-03-04 19:46:33 -0500

    Format ts file with prettier

--- src/error.ts
+++ src/error.ts
@@ -3,4 +3,8 @@
  * `Errors` type accounts for errors in Node, but some failure cases can happen
  * in the called Elm code.
  */
-export type Errors = "MalformedCLIArgs" | "ElmProjectFileNotFound" | "UnsupportedElmVersion" | "WritingOutputFailed";
+export type Errors =
+  | "MalformedCLIArgs"
+  | "ElmProjectFileNotFound"
+  | "UnsupportedElmVersion"
+  | "WritingOutputFailed";
