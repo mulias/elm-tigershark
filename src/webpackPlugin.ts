23982950570eb618736bc8e1d503e8a35aa5a1f6
Elias Mulhall <eli.mulhall@gmail.com>
2020-03-06 15:54:39 -0500

    [webpack] Generate types once when webpack initializes

--- src/webpackPlugin.ts
+++ src/webpackPlugin.ts
@@ -23,7 +23,25 @@ export class ElmTigersharkPlugin {
     this.command = command;
   }
 
+  runCommand = () => {
+    this.process = exec(this.command, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`error running tigershark:\n ${err}`);
+      } else {
+        console.log(`tigershark completed with stdout:\n ${stdout}`);
+        console.warn(`tigershark completed with stderr:\n ${stderr}`);
+      }
+      this.process = null;
+    });
+  };
+
   apply(compiler: Compiler) {
+    // this should happen right before webpack runs in non-watch mode
+    compiler.hooks.beforeRun.tap("ElmTigersharkPlugin", (_: Compiler) => {
+      this.runCommand();
+    });
+
+    // this should happen every time changes are made in watch mode
     compiler.hooks.watchRun.tapAsync(
       "ElmTigersharkPlugin",
       (passedCompiler: Compiler, done: () => void) => {
@@ -37,17 +55,8 @@ export class ElmTigersharkPlugin {
           this.process.kill();
         }
 
-        this.process = exec(this.command, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`error running tigershark:\n ${err}`);
-          } else {
-            console.log(`tigershark completed with stdout:\n ${stdout}`);
-            console.warn(`tigershark completed with stderr:\n ${stderr}`);
-          }
-          this.process = null;
-        });
-
-        return done();
+        this.runCommand();
+        done();
       }
     );
   }
