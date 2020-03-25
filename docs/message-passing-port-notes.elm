06927f8ee6d52cae441ee791af5da6f8778202e0
Elias Mulhall <eli.mulhall@gmail.com>
2020-03-25 00:38:54 -0400

    Update notes on higher level type interop

--- docs/message-passing-port-notes.elm
+++ docs/message-passing-port-notes.elm
@@ -1,13 +1,42 @@
 port module Main exposing (Direction(..), Message(..))
 
+{-| Here's a code sketch inspired by <https://discourse.elm-lang.org/t/generating-type-safe-ports-from-custom-types/1841>
+In addition to the primitive types which can be auto-converted through ports
+and flags, there are two kinds of types which are conceptually comparable
+between Elm and TS, but require conversion via encoder/decoder pairs.
+
+The first kind of type is an "enum" type, created in Elm as a custom type where
+each variant takes no type arguments, and created in TS as a union of string
+literals.
+
+The second kind of type is a "labeled record", created in Elm as a custom type
+where each variant takes a record, and created in TS as a discriminated union.
+
+It should be possible to facilitate this kind of higher level type interop by
+generating the TS types for qualifying Elm types, and also generating the codec
+functions to pass the types back and forth.
+
+Open questions:
+
+  - Where do the generated Elm codecs go
+  - Do the types really need an `@elm-tigershark-type` annotation?
+  - Do annotations stay in the doc comments added to the TS declarations
+  - How should types be scoped?
+
+-}
+
 import Json.Decode as JD
 import Json.Encode as JE
 
 
+{-| Add the annotation
 
-{- Given the types -}
+@elm-tigershark-type
 
+in a doc comment to indicate that code should be generated to make this type
+interoperable.
 
+-}
 type Direction
     = North
     | South
@@ -15,30 +44,61 @@ type Direction
     | West
 
 
+{-| @elm-tigershark-type
+-}
 type Message
     = Foo { fieldA : String, fieldB : Int }
     | Bar { x : Bool, y : Direction }
 
 
-{-| @elm-tigershark use Message
+{-| Indicate what type a flags json value will decode to with
 
-We annotate the port with the Elm type which will be sent through the port.
+@elm-tigershark-flags-using Direction
+
+-}
+main : Program JD.Value model msg
+main =
+    Debug.todo "write main"
+
+
+{-| Indicate what type a port json value will encode or decode to with
+
+@elm-tigershark-port-using Message
 
 -}
 port sendMessage : JE.Value -> Cmd msg
 
 
 
-{- This generates corresponding TypeScript types
+{- This generates corresponding TypeScript types, and sets the type of the
+   ports and flags appropriately
 
-      type Direction = "Noth" | "South" | "East" | "West"
+   declare module "*.elm" {
+     export namespace Elm {
+       namespace Main {
 
-      type Message =
-          | { kind: "Foo"; fieldA: string; fieldB: number }
-          | { kind: "Bar"; x: boolean; y: Direction }
+         type Direction = "Noth" | "South" | "East" | "West"
 
+         type Message =
+           | { kind: "Foo"; fieldA: string; fieldB: number }
+           | { kind: "Bar"; x: boolean; y: Direction }
 
-   And also creates encoder/decoder pairs:
+         export interface App {
+           ports: {
+             sendMessage: {
+               subscribe(callback: (data: Message) => void): void;
+             };
+           };
+           export function init(options: {
+             node?: HTMLElement | null;
+             flags: Direction;
+           }): Elm.Main.App;
+         }
+       }
+     }
+   }
+
+   And also creates Elm encoder/decoder pairs:
 -}
 
 
