861ee32e1238bfdbfe5edc7fc836e4b69a25302b
Elias Mulhall <eli.mulhall@gmail.com>
2020-04-01 23:30:31 -0400

    Add approval testing framework

---
+++ tests-approval/expectSuccess/simple/src/Main.d.ts
@@ -0,0 +1,21 @@
+// WARNING: Do not manually modify this file. It was generated using:
+// https://github.com/mulias/elm-tigershark
+// Type definitions for using Elm programs in TypeScript
+
+export namespace Elm {
+  namespace Main {
+    export interface App {
+      ports: {
+        fromElm: {
+          subscribe(callback: (data: string) => void): void;
+        };
+        toElm: {
+          send(data: string): void;
+        };
+      };
+    }
+    export function init(options: {
+      node?: HTMLElement | null;  
+    }): Elm.Main.App;
+  }
+}
